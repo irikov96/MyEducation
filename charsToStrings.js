c90181ab1e7404dae7c94aab5f9d66a74eadecd0
Anton Irikov <106865372+irikov96@users.noreply.github.com>
2022-07-18 20:54:33 +0300

    Add files via upload
    Data Types and Variables

---
+++ charsToStrings.js
@@ -0,0 +1,4 @@
+function charsToStrings(char1, char2, char3){
+console.log(`${char1}${char2}${char3}`);
+}
+charsToStrings('a','b','c')
---
+++ poundsToDollars.js
@@ -0,0 +1,4 @@
+function poundsToDollars(dollars){
+console.log((dollars*1.31).toFixed(3));
+}
+poundsToDollars(80)
